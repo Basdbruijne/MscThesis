88927b6db98382af720373b41938321ebf3bfbbe
Basdbruijne <basdbruijne@gmail.com>
2020-07-31 13:59:37 +0200

    Update AO_Functions.py

--- AO_Functions.py
+++ AO_Functions.py
@@ -8,7 +8,6 @@ except:
 def convolve2(a, b):
     # Check the type of input (numpy/cupy)
     cupy = "cupy" in str(a.data)
-    
     if not cupy:
         a = np.array(a)
         b = np.array(b)
@@ -21,22 +20,24 @@ def convolve2(a, b):
     a_pad = np.pad(a, [0, b.shape[0]-1], mode='constant')
     b_pad = np.pad(b, [0, a.shape[0]-1], mode='constant')
     
-    
+    # Convolve the image and crop the edges
     edge = int(np.minimum(a.shape[0], b.shape[0])/2)
     c = np.real(np.fft.ifft2(np.fft.fft2(a_pad)*np.fft.fft2(b_pad)))[edge:-edge, edge:-edge]
     
+    # Convert back to numpy if needed
     if not cupy:
         c = np.asnumpy(c)
         
     return c
 
 def deconvolve(image, psf, mode = 'LR', iterations=10):
+    # Check the type of input (numpy/cupy)
     cupy = "cupy" in str(image.data)
-    
     if not cupy:
         image = np.array(image)
         psf = np.array(psf)
     
+    # Make sure matrices are square
     if image.shape[0] != image.shape[1] or psf.shape[0] != psf.shape[1]:
         raise Exception('Non-square images are not (yet) supported')
         
@@ -51,29 +52,30 @@ def deconvolve(image, psf, mode = 'LR', iterations=10):
     psf_mirror_F = np.fft.fft2(np.pad(psf_mirror, [image.shape[0]-1, 0], mode='constant'))
     o_F = np.fft.fft2( np.pad(objectt, [0, psf.shape[0]-1], mode='constant')) #uncomment for zero intial guess
 
+    # Perform the deconvolution
     if mode == 'LR':
         tau = 1.5
         for n in range(iterations):
             d = (i_F-np.multiply(psf_F, o_F))*psf_mirror_F
             o_F += tau*d
-            
     elif mode == 'Regularization':
         if iterations != 10:
             print('Regularization mode does not work iteratively')
-            
         o_F = i_F/(psf_F+1e-4)
-
     else:
         raise Exception('Deconvolution method not recognized')
     
+    # Transform the object back
     output = np.abs(np.fft.ifft2(o_F))
     
+    # Convert back to numpy if needed
     if not cupy:
         output = np.asnumpy(output)
         
     return output
 
 def fact(x):
+    # Return the factorial of a variable
     if x%1:
         print('Input is not an integer')
     a = 1
